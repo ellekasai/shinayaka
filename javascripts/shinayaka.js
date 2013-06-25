1fb4d1ef0338028fa1fea3da8b43bab48691d2c9
Elle Kasai <elle.kasai@gmail.com>
2013-06-25 10:27:44 -0700

    fix js

--- javascripts/shinayaka.js
+++ javascripts/shinayaka.js
@@ -3,4 +3,10 @@ $(function() {
     $(".js-article-logs").show();
     $(".js-show-article-logs").addClass("activated");
   });
+
+  $(".js-header-archive-link").click(function() {
+    $(".js-article-logs").show();
+    $(".js-show-article-logs").addClass("activated");
+    window.location.hash = "archive";
+  });
 });
